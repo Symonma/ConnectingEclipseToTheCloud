c47714e9836d6eeab9d06d965bf1c32fc4c72996
Symonma <symonntma@gmail.com>
2017-02-01 12:29:58 -0600

    Yet another!!!

--- src/eClipseToCloud/Driver.java
+++ src/eClipseToCloud/Driver.java
@@ -4,11 +4,11 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-<<<<<<< HEAD
+
 		//I really want Cane's for lunch
-=======
+
 		// Good job Symon
 	}
->>>>>>> 5f74a396e095d45a1569d0a1f3e92061d4d9e26f
+
 
 }
